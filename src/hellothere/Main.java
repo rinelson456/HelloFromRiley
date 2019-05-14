e0d1bfe3ac2092083ae094fd0ae30275c8481200
jordanburdett <jordan@burdett.us>
2019-05-14 13:42:36 -0600

    said hi...

--- src/hellothere/Main.java
+++ src/hellothere/Main.java
@@ -3,6 +3,8 @@ package hellothere;
 public class Main {
 
     public static void main(String[] args) {
-	   System.out.println("Hello my name is Riley");
+
+        System.out.println("Hello my name is Riley");
+        System.out.println("Hello Riley, my name is Jordan");
     }
 }
